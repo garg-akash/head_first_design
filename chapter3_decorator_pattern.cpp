f56d8ba4545cfc3b41ec7e40e75f3a72a22fa2f2
garg-akash <akashgrg2@gmail.com>
2023-03-26 09:35:59 +0200

    description not correct

---
+++ chapter3_decorator_pattern.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+
+class Beverage
+{
+public:
+	std::string description;
+	Beverage()
+	{
+		description = "unknown Beverage";
+	}
+	~Beverage() {}
+	std::string getDescription()
+	{
+		return description;
+	}
+
+	virtual double cost() = 0;
+	// double cost()
+	// {
+	// 	return 0;
+	// }
+};
+
+class CondimentDecorator : public Beverage
+{
+public:
+	virtual std::string getDescription() = 0;
+};
+
+class Espresso : public Beverage
+{
+public:
+	Espresso()
+	{
+		description = "Espresso";
+	}
+	double cost()
+	{
+		return 1.99;
+	}
+};
+
+class HouseBlend : public Beverage
+{
+public:
+	HouseBlend()
+	{
+		description = "HouseBlend";
+	}
+	double cost()
+	{
+		return 0.89;
+	}
+};
+
+class Mocha : public CondimentDecorator
+{
+private: Beverage* beverage;
+public:
+	Mocha(Beverage* b)
+	{
+		beverage = b;
+		description = b->getDescription() + ", Moch"; // this is fine
+	}
+	~Mocha() {}
+	std::string getDescription()
+	{
+		std::cout << beverage->getDescription() << "!!\n";
+		return beverage->getDescription() + ", Mocha";
+	}
+
+	double cost()
+	{
+		return beverage->cost() + 0.20;
+	}
+};
+
+class Whip : public CondimentDecorator
+{
+private: Beverage* beverage;
+public:
+	Whip(Beverage* b)
+	{
+		beverage = b;
+		// description = "Moch";
+	}
+	~Whip();
+	std::string getDescription()
+	{
+		return beverage->getDescription() + ", Whip";
+	}
+
+	double cost()
+	{
+		return beverage->cost() + 0.40;
+	}
+};
+
+int main(int argc, char const *argv[])
+{
+	Beverage* b1 = new Espresso();
+	Beverage* b2 = new Mocha(b1);
+	std::cout << b2->getDescription() << "\n"; // this calls getDescription of Parent class -- issue
+	std::cout << b2->cost() << "\n";  
+	return 0;
+}
